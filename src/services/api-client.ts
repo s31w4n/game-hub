3d38910591fdf4e5511aaa2eb7163e87547ea116
s31w4n <sewan.khalni@gmail.com>
2024-02-02 00:28:15 +0330

    Adding apiKey to .env file

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '84f85609fd634c2084fe02628868d7ea',
+    key: import.meta.env.VITE_API_KEY,
   },
 });
