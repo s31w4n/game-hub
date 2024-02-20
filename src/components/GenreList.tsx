6ee42dae8186dac185c67c88d7f034c427395a8b
s31w4n <sewan.khalni@gmail.com>
2024-02-20 21:54:24 +0330

    Refactor: fetch genres and platforms with react query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -17,6 +17,7 @@ interface Props {
 
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, error, isLoading } = useGenres();
+
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   if (error) return null;
@@ -29,7 +30,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
       <List>
         {isLoading &&
           skeletons.map((skeleton) => <GenreListSkeleton key={skeleton} />)}
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
