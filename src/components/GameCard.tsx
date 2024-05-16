4d101706630b25f5ac7efe1a8e8355483b71c8c1
s31w4n <sewan.khalni@gmail.com>
2024-05-16 23:52:35 +0330

    Fetch game details

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedUrl from '../services/image-url';
 import Emoji from './Emoji';
+import { Link } from 'react-router-dom';
 
 interface Props {
   game: Game;
@@ -20,8 +21,14 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Flex flexDirection='column' justifyContent='space-between' height='98px'>
-          <Heading fontSize="2xl">{game.name}</Heading>
+        <Flex
+          flexDirection="column"
+          justifyContent="space-between"
+          height="98px"
+        >
+          <Heading fontSize="2xl">
+            <Link to={`/games/${game.slug}`}>{game.name}</Link>
+          </Heading>
           <Emoji rating={game.rating_top} />
         </Flex>
       </CardBody>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -6,10 +6,12 @@ import useGameQueryStore from '../store';
 export interface Game {
   id: number;
   name: string;
+  slug: string;
   metacritic: number;
+  rating_top: number;
+  description_raw: string
   background_image: string;
   parent_platforms: { platform: Platform }[];
-  rating_top: number;
 }
 
 const apiClient = new ApiClient<Game>('/games');
