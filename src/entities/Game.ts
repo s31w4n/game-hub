4dff5d64087c936c417d47a069e9f5f3e3d64cef
s31w4n <sewan.khalni@gmail.com>
2024-05-17 12:22:05 +0330

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,11 +1,15 @@
-import { Platform } from "./Platform";
+import { Genre } from './Genre';
+import { Platform } from './Platform';
+import { Publisher } from './Publisher';
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
   metacritic: number;
   rating_top: number;
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
